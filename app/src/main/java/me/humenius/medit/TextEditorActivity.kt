25c4477028fdd3643a29340f4b5d3b000f5f8202
kereis <kreis-dev@gmx.net>
2021-01-08 10:09:49 +0100

    Add project files

---
+++ app/src/main/java/me/humenius/medit/TextEditorActivity.kt
@@ -0,0 +1,11 @@
+package me.humenius.medit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextEditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editor)
+    }
+}
