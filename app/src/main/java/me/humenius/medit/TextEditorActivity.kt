9eec8609aef9ae7090dea1b41ca7a7a14ca66d32
kereis <kreis-dev@gmx.net>
2021-01-25 07:49:07 +0100

    Add basic editor and custom toolbar tailored for Markdown

--- app/src/main/java/me/humenius/medit/TextEditorActivity.kt
+++ app/src/main/java/me/humenius/medit/TextEditorActivity.kt
@@ -2,10 +2,42 @@ package me.humenius.medit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.dankito.richtexteditor.android.RichTextEditor
+import net.dankito.richtexteditor.android.toolbar.EditorToolbar
+import net.dankito.utils.android.permissions.IPermissionsService
+import net.dankito.utils.android.permissions.PermissionsService
 
 class TextEditorActivity : AppCompatActivity() {
+
+    private lateinit var editor: RichTextEditor
+    private lateinit var toolbar: EditorToolbar
+    private var permissionsService: IPermissionsService = PermissionsService(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
+
+        setupEditor()
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        toolbar = findViewById(R.id.editorToolbar)
+        toolbar.editor = editor
+    }
+
+    private fun setupEditor() {
+        editor = findViewById(R.id.editor)
+        editor.permissionsService = permissionsService
+
+        editor.setEditorFontFamily("sans-serif")
+        editor.setEditorFontSize(20)
+        editor.setPadding(4 * (resources.displayMetrics.density.toInt()))
+    }
+
+    override fun onBackPressed() {
+        if (!toolbar.handlesBackButtonPress()) {
+            super.onBackPressed()
+        }
     }
 }
