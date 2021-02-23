827deb7f9b0b9916aa2428eb31488e7150e83320
kereis <kreis-dev@gmx.net>
2021-02-23 11:24:11 +0100

    Abstract fragments

--- app/src/main/java/me/humenius/medit/MainActivity.kt
+++ app/src/main/java/me/humenius/medit/MainActivity.kt
@@ -1,16 +1,9 @@
 package me.humenius.medit
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
 import androidx.fragment.app.commit
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import me.humenius.medit.editor.EditorFragment
+import me.humenius.medit.ui.editor.EditorFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/me/humenius/medit/ui/editor/AbstractFragment.kt
@@ -0,0 +1,34 @@
+package me.humenius.medit.ui.editor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * <h1>AbstractFragment</h1>
+ * <p>A base class for handling fragments
+ *
+ * @see Fragment
+ * @author humenius
+ * @version 1.0.0
+ */
+abstract class AbstractFragment(layoutId: Int) : Fragment(layoutId) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = super.onCreateView(inflater, container, savedInstanceState)
+
+        initView()
+
+        return view
+    }
+
+    /**
+     * Initializes view
+     */
+    abstract fun initView()
+}
