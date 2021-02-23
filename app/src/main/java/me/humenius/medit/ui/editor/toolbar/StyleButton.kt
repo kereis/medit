d4bee2a6a08e39ade4ba278f83c570206f31c786
kereis <kreis-dev@gmx.net>
2021-02-23 11:36:23 +0100

    Remove deprecated classes

--- app/src/main/java/me/humenius/medit/ui/editor/toolbar/StyleButton.kt
+++
@@ -1,3 +0,0 @@
-package me.humenius.medit.ui.editor.toolbar
-
-class StyleButton(var icon: Int, var id: Int)
--- app/src/main/java/me/humenius/medit/ui/editor/toolbar/StyleToolbarAdapter.kt
+++
@@ -1,8 +0,0 @@
-package me.humenius.medit.ui.editor.toolbar
-
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.button.MaterialButton
-
-//class StyleToolbarAdapter(private var buttonsList: List<StyleButton>) : RecyclerView.Adapter<StyleToolbarAdapter.ViewHolder>() {
-//    class ViewHolder(val styleButton: MaterialButton) : RecyclerView.ViewHolder(styleButton)
-//}
