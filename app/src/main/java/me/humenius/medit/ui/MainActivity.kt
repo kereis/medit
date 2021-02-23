c98919ff943030cec64479641884f19a89192a0c
kereis <kreis-dev@gmx.net>
2021-02-23 11:38:24 +0100

    Move MainActivity to ui package

--- app/src/main/java/me/humenius/medit/ui/MainActivity.kt
+++ app/src/main/java/me/humenius/medit/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package me.humenius.medit
+package me.humenius.medit.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import me.humenius.medit.R
 import me.humenius.medit.ui.editor.EditorFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
