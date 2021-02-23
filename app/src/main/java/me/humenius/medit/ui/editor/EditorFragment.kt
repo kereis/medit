f156a9d9b2d771819d608ad7a6ed2735f595c264
kereis <kreis-dev@gmx.net>
2021-02-23 11:24:11 +0100

    Abstract fragments

---
+++ app/src/main/java/me/humenius/medit/ui/editor/EditorFragment.kt
@@ -0,0 +1,12 @@
+package me.humenius.medit.ui.editor
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import me.humenius.medit.R
+
+class EditorFragment : AbstractFragment(R.layout.fragment_editor) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+}
