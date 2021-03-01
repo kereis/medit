7ea08eb332efcaf34b9e45651595d47a68cc6562
kereis <kreis-dev@gmx.net>
2021-03-01 13:40:01 +0100

    Split different text action types into MarkdownTextActions enum

---
+++ app/src/main/java/me/humenius/medit/format/markdown/BoldTextAction.kt
@@ -0,0 +1,14 @@
+package me.humenius.medit.format.markdown
+
+import me.humenius.medit.editor.InlineSpanTextAction
+
+/**
+ * **BoldTextAction**
+ *
+ * Applies bold text formatting at current selection.
+ *
+ * @author humenius
+ * @see InlineSpanTextAction
+ * @version 1.0.0
+ */
+// class BoldTextAction : InlineSpanTextAction("**")
---
+++ app/src/main/java/me/humenius/medit/format/markdown/MarkdownTextActions.kt
@@ -0,0 +1,13 @@
+package me.humenius.medit.format.markdown
+
+import me.humenius.medit.editor.BlockTextAction
+import me.humenius.medit.editor.InlineSpanTextAction
+import me.humenius.medit.editor.TextAction
+
+enum class MarkdownTextActions(val textAction: TextAction) {
+    BOLD(InlineSpanTextAction("**")),
+    ITALIC(InlineSpanTextAction("*")),
+    INLINE_CODE(InlineSpanTextAction("`")),
+
+    CODE_BLOCK(BlockTextAction("```")), QUOTE(BlockTextAction(">"))
+}
