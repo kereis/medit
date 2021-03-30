75944a8b25f765680f363f6044fa24be28fb04d7
kereis <kreis-dev@gmx.net>
2021-03-30 08:34:50 +0200

    feat(dep-inject): Add Dagger, Hilt and AppModule containing AppDatabase

---
+++ plugins-android/src/main/kotlin/com/github/kereis/medit/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.github.kereis.medit.di
+
+import android.content.Context
+import com.github.kereis.medit.plugins.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRoomInstance(
+        @ApplicationContext context: Context
+    ) = AppDatabase.getInstance(context)
+
+    @Singleton
+    @Provides
+    fun provideRecentFilesDao(db: AppDatabase) = db.recentFilesDao()
+}
