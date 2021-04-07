a3d68805f9e93e10cda8b2f101aea5e264d8d731
kereis <kreis-dev@gmx.net>
2021-04-07 14:23:06 +0200

    test(plugins-android-database): Correct package path
    This should fix the CI "a little bit"

--- plugins-android-database/src/androidTest/java/com/github/kereis/medit/plugins/database/ExampleInstrumentedTest.kt
+++ plugins-android-database/src/androidTest/java/com/github/kereis/medit/plugins/database/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kereis.github.medit.plugins.database.test", appContext.packageName)
+        assertEquals("com.github.kereis.medit.plugins.database.test", appContext.packageName)
     }
 }
