1ba716a0eb05324ba129b45d76fd57292d1e77be
kereis <kreis-dev@gmx.net>
2021-05-26 13:34:56 +0200

    feat(math-expressions): Implement LaTeX support for used markdown parser

--- plugins-android/src/main/kotlin/com/github/kereis/medit/ui/editor/MarkdownRenderFragment.kt
+++ plugins-android/src/main/kotlin/com/github/kereis/medit/ui/editor/MarkdownRenderFragment.kt
@@ -1,24 +1,29 @@
 package com.github.kereis.medit.ui.editor
 
-import android.text.Spanned
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import com.github.kereis.medit.R
 import com.github.kereis.medit.databinding.FragmentPreviewBinding
+import com.github.kereis.medit.parser.MarkdownParserFactory
 import com.github.kereis.medit.ui.BaseFragment
+import io.noties.markwon.Markwon
+import timber.log.Timber
 
-class MarkdownRenderFragment(private val textToDisplay: Spanned) :
+class MarkdownRenderFragment(private val textToDisplay: String) :
     BaseFragment<FragmentPreviewBinding>(R.layout.fragment_preview) {
 
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean)
     -> FragmentPreviewBinding = FragmentPreviewBinding::inflate
 
+    private lateinit var markdownParser: Markwon
+
     override fun initView() {
         initToolbar()
 
-        binding.previewTextView.text = textToDisplay
+        markdownParser =
+            MarkdownParserFactory.newInstance(requireContext(), binding.previewTextView.textSize)
+        markdownParser.setMarkdown(binding.previewTextView, textToDisplay)
     }
 
     private fun initToolbar() {
@@ -34,7 +39,7 @@ class MarkdownRenderFragment(private val textToDisplay: Spanned) :
 
             toolbar = newToolbar
         } else {
-            Log.println(Log.WARN, javaClass.name, "Could not initialize toolbar.")
+            Timber.w("Could not initialize toolbar.")
         }
     }
 }
